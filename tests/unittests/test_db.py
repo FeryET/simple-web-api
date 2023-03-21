24683ddb6d61a6cc26f804899c9dcaaf21593765
Farhood Etaati <farhoodetaati@gmail.com>
2023-03-21 20:33:30 +0330

    adds autoflake

--- tests/unittests/test_db.py
+++ tests/unittests/test_db.py
@@ -1,8 +1,6 @@
-import pytest
 from sqlalchemy.orm import Session
 
 from simple_web_api.db import DB_ENGINE
-from simple_web_api.model import ItemModel
 
 
 def test_db_connect(db_session: Session):
